9e9342478a7cce8cc4ddce1e7a9e0c056ad07290
srtushar887 <sr.tushar.pro@gmail.com>
2021-08-15 17:09:09 +0600

    use state

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React,{useState} from 'react';
 
 import {
   Button,
@@ -30,10 +30,25 @@ import {
 
 
 const App = () => {
+  const [name, setName] = useState('tusher');
+  const [session, setSession] = useState({number : 6,title:'state'})
+  const [curent, setCurent] = useState(true);
+
+  
+
+const onClickHandler= ()=>{
+  setName('SRT');
+  setSession({number:7,title:'Style'});
+  setCurent(false);
+
+}
+
  return(
     <View style={styles.body}>
-       <Text style={styles.text}>Programming With SRT</Text> 
-       <Button title='youtube chanel' onPress={()=>{Linking.openURL('https://www.facebook.com/')}}></Button>
+       <Text style={styles.text}>Programming With {name}</Text> 
+       <Text style={styles.text}>This session state is  {session.number} and about is {session.title}</Text> 
+       <Text style={styles.text}>{curent ? 'current session ' : 'next session'}</Text> 
+       <Button title='update state' onPress={onClickHandler}></Button>
     </View>
  );
 };
