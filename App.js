2d75df9c8140fd20c3fcd14eda5565c34f5834af
srtushar887 <sr.tushar.pro@gmail.com>
2021-08-23 01:51:05 +0600

    textinput

--- App.js
+++ App.js
@@ -18,6 +18,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
 } from 'react-native';
@@ -32,70 +33,26 @@ import {
 
 
 const App = () => {
- 
-  const [Items,setItems] = useState([
-    {key:1,name:'item 1'},
-    {key:2,name:'item 2'},
-    {key:3,name:'item 3'},
-    {key:4,name:'item 4'},
-    {key:5,name:'item 5'},
-    {key:6,name:'item 6'},
-    {key:7,name:'item 7'},
-    {key:8,name:'item 8'},
-    {key:9,name:'item 9'},
-    {key:10,name:'item 10'},
-    {key:11,name:'item 11'},
-  ])
-
-  const [Refreshing, setRefreshing] = useState(false)
-
-  const onRefresh = ()=>{
-    setRefreshing(true);
-    setItems([...Items,{key:12,name:'item 12'}]);
-    setRefreshing(false)
-  }
-
-
-
+  
+  const [name, setName] = useState('');
+  
  return(
+   <View style={styles.body}>
+     <Text style={styles.text}>Please write your name</Text>
+     <TextInput 
+    //  multiline
+     style={styles.input}
+     placeholder='e.g Jhon'
+     onChangeText={(value)=>setName(value)}
+     keyboardType='default'
+    //  maxLength={2}
+    // editable={false}
+    // secureTextEntry
+     ></TextInput>
+      <Text style={styles.text}>{name}</Text>
+   </View>
+ )
 
-
-  <FlatList
-    // horizontal
-    data={Items}
-    keyExtractor={(item,index)=>index}
-    renderItem={({item})=>(
-      <View style={styles.item}>
-        <Text style={styles.text} >{item.name}</Text>
-       </View>
-    )}
-     refreshControl={
-    <RefreshControl 
-    refreshing={Refreshing}
-    onRefresh={onRefresh}
-    />
-     }
-
-  />
-
-
-  // <ScrollView 
-  // horizontal={false} 
-  // style={styles.body}
-  // refreshControl={
-  //   <RefreshControl 
-  //   refreshing={Refreshing}
-  //   onRefresh={onRefresh}
-  //   />
-  // }
-  // >
-  // {Items.map((name)=>{
-  //    return <View style={styles.item} key={name.ket}>
-  //      <Text style={styles.text} >{name.item}</Text>
-  //     </View>
-  //   })}
-  // </ScrollView>
- );
 };
 
 const styles = StyleSheet.create({
@@ -103,21 +60,24 @@ const styles = StyleSheet.create({
     flex:1,
     flexDirection:'column',
     backgroundColor:'#ffffff',
-  },
-  item:{
-    margin:10,
-    backgroundColor:'#4ae1fa',
-    justifyContent:'center',
     alignItems:'center'
   },
 
   text:{
-    color:'#fff',
-    fontSize:35,
-    fontStyle:'italic',
-    margin:10,
-  
+      color:'#000000',
+      fontSize:20,
+      margin:20
   },
+
+  input:{
+    width:200,
+    borderWidth:1,
+    borderColor:'#555',
+    borderRadius:5,
+    textAlign:'center',
+    fontSize:20
+  }
+  
 
 
 });
